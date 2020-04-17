46c12822a5dae178a04996e0ca4de7802a730f4a
Wender <wendergalan2014@hotmail.com>
2020-04-16 20:26:46 -0400

    Finalizando as alterações no docker-compose.yml

--- front-end/person/src/plugins/axios.js
+++ front-end/person/src/plugins/axios.js
@@ -8,7 +8,8 @@ const token = Buffer.from(`${username}:${password}`, 'utf8').toString('base64')
 Vue.use({
   install(Vue) {
     Vue.prototype.$http = axios.create({
-      baseURL: 'https://person-vue-spring-boot.herokuapp.com',
+      // Ou irá pegar a URL do netlify ou acessar a API do docker
+      baseURL: process.env.API_URL || 'http://localhost:4000',
       headers: {
         Authorization: `Basic ${token}`
       }
